0d1669846df21b88217d31c396112bddf226490e
Etienne <livayglound@gmail.com>
2024-11-01 21:10:17 +0000

    commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -17,7 +17,6 @@ class RegisteredUserController extends Controller
 
     public function store(Request $request)
     {
-        // Validation des données entrantes
         $validatedData = $request->validate([
             'prenom' => ['required', 'string', 'max:255', 'regex:/^[\S]+(\s[\S]+)*$/'],
             'nom' => ['required', 'string', 'max:255', 'regex:/^[\S]+(\s[\S]+)*$/'],
@@ -26,42 +25,44 @@ class RegisteredUserController extends Controller
             'adresse' => 'required|string|max:255',
             'cni' => 'required|string|max:20',
             'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'password' => [
+            'mot_de_passe' => [
                 'required',
                 'string',
                 'min:8',
                 'regex:/^[A-Za-z0-9@]+$/',
                 'confirmed',
             ],
-            'role' => 'required|in:client,administrateur',
+            'role' => 'required|in:client,distributeur',
         ]);
-    
+
         // Gestion du téléchargement de la photo
         $photoPath = null;
-    
+
         if ($request->hasFile('photo')) {
             $fileName = time() . '_' . $request->file('photo')->getClientOriginalName();
             $request->file('photo')->move(public_path('photos'), $fileName);
             $photoPath = 'photos/' . $fileName;
         }
-    
+
+        $validatedData['photo'] = $photoPath;
+
         // Création de l'utilisateur
         User::create([
-            'nom' => $validatedData['nom'],
-            'prenom' => $validatedData['prenom'],
-            'date_naissance' => $validatedData['date_naissance'],
-            'telephone' => $validatedData['telephone'],
-            'adresse' => $validatedData['adresse'],
-            'role' => $validatedData['role'],
-            'statut' => 0,
-            'password' => Hash::make($validatedData['mot_de_passe']),
-            'cni' => $validatedData['cni'], // Conserver le type de données string
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'email' => $request->email,
+            'date_naissance' => $request->date_naissance,
+            'telephone' => $request->telephone,
+            'adresse' => $request->adresse,
+            'mot_de_passe' => Hash::make($request->mot_de_passe),
+            'role' => $request->role,
+            'cni' => (int)$request->cni,
             'date_creation' => now(),
             'photo' => $photoPath,
         ]);
-        
+
         // Redirection ou réponse après la création avec un message de succès
-        return redirect()->back()->with('success', 'Utilisateur créé avec succès.');
+        return redirect()->route('register')->with('success', 'Utilisateur créé avec succès.');
     }
 
 }
